b60a5eeacfbce92aa21d6ac190cf28ce9b43bf12
Matthew Mamonov <g.smellyshovel@gmail.com>
2022-04-08 02:17:33 +0400

    feat: WIP on the 3rd example

--- demo/src/examples/3 Mapbox Directions API and Congestions.vue
+++ demo/src/examples/3 Mapbox Directions API and Congestions.vue
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="../assets/styles/index.css">
 <template>
   <app-sidebar>
     <template #title>{{ name }}</template>
@@ -10,14 +11,18 @@
       </select>
     </label>
 
-    <p>This example makes POST requests to the official Mapbox Directions API and enables the following options:</p>
+    <p>This example makes POST requests to the official Mapbox Directions API with the following options:</p>
 
     <ul>
-      <li></li>
+      <li>
+        <strong>annotations={{ annotations }}</strong>
+      </li>
+      <li>overview=full</li>
+      <li>alternatives=true</li>
     </ul>
   </app-sidebar>
 
-  <div ref="mapRef" class="shadow-xl" />
+  <div ref="mapRef" :key="annotations" class="shadow-xl" />
 </template>
 
 <script setup lang="ts">
@@ -35,37 +40,35 @@
   const directions = ref<MaplibreGlDirections>();
 
   const annotations = ref("congestion");
-  watch(annotations, () => {
-    directions.value?.destroy();
-    directions.value.init();
-  });
 
   const mapRef = ref();
-  onMounted(() => {
-    const _map = new maplibregl.Map({
-      container: mapRef.value,
-      style,
-      center: [-74.1197632, 40.6974034],
-      zoom: 11,
-    });
-
-    _map.on("load", () => {
-      map.value = _map;
-
-      directions.value = new MaplibreGlDirections(map.value, {
-        api: "https://api.mapbox.com/directions/v5",
-        profile: "mapbox/driving-traffic",
-        makePostRequest: true,
-        requestOptions: {
-          access_token:
-            "pk.eyJ1Ijoic21lbGx5c2hvdmVsIiwiYSI6ImNsMWtoMm9najAwcjczb21vdm9hdjQyOXMifQ.oThCjV-a5XLNI0Ly-FEZXQ",
-          alternatives: "true",
-          annotations: annotations.value,
-          overview: "full",
-        },
+  watch(mapRef, () => {
+    if (mapRef.value) {
+      const _map = new maplibregl.Map({
+        container: mapRef.value,
+        style,
+        center: [-74.1197632, 40.6974034],
+        zoom: 11,
       });
 
-      directions.value.interactive = true;
-    });
+      _map.on("load", () => {
+        map.value = _map;
+
+        directions.value = new MaplibreGlDirections(map.value, {
+          api: "https://api.mapbox.com/directions/v5",
+          profile: "mapbox/driving-traffic",
+          makePostRequest: true,
+          requestOptions: {
+            access_token:
+              "pk.eyJ1Ijoic21lbGx5c2hvdmVsIiwiYSI6ImNsMW80eXQ4aTEwN3czcG8zMXM2NzJ2ODIifQ.oWHUIUiEj2SS4_hue3qv2g",
+            alternatives: "true",
+            annotations: annotations.value,
+            overview: "full",
+          },
+        });
+
+        directions.value.interactive = true;
+      });
+    }
   });
 </script>
