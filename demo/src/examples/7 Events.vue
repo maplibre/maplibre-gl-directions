01a9dd6107db12eda365fc91f6bf454476ca2ca2
Matthew Mamonov <g.smellyshovel@gmail.com>
2022-04-17 23:56:18 +0400

    Introduce a Demo for Events

---
+++ demo/src/examples/7 Events.vue
@@ -0,0 +1,69 @@
+<template>
+  <app-sidebar>
+    <template #title>{{ name }}</template>
+
+    <p>
+      This example listens for all the available events and logs them below. Interact with the map to see the emitted
+      events.
+    </p>
+
+    <ol>
+      <li v-for="message in messages" :key="message" v-html="message" />
+    </ol>
+
+    <button v-if="messages.length" @click="messages = []">Clear</button>
+  </app-sidebar>
+
+  <div ref="mapRef" class="shadow-xl" />
+</template>
+
+<script setup lang="ts">
+  import { onMounted, ref } from "vue";
+  import { useRoute } from "vue-router";
+  import AppSidebar from "../components/AppSidebar.vue";
+  import maplibregl from "maplibre-gl";
+  import "maplibre-gl/dist/maplibre-gl.css";
+  import style from "../assets/map/style/style.json?url";
+  import MaplibreGlDirections from "@maplibre/maplibre-gl-directions";
+
+  const name = ref(useRoute().matched[0].name);
+
+  const directions = ref<MaplibreGlDirections>();
+
+  const messages = ref<string[]>([]);
+
+  const mapRef = ref();
+  onMounted(() => {
+    const map = new maplibregl.Map({
+      container: mapRef.value,
+      style,
+      center: [-74.1197632, 40.6974034],
+      zoom: 11,
+      customAttribution: "<a href='http://project-osrm.org/' target='_blank'>&copy; OSRM</a>",
+    });
+
+    map.on("load", () => {
+      directions.value = new MaplibreGlDirections(map, {
+        requestOptions: {
+          alternatives: "true",
+        },
+      });
+
+      directions.value.interactive = true;
+
+      directions.value?.on("addwaypoint", (e) => {
+        console.log(e);
+        messages.value.push(
+          `<strong>${e.type}</strong>: waypoint added at index <strong>${e.index}</strong>. Original event - <strong>${e.originalEvent.type}</strong>`,
+        );
+      });
+
+      directions.value?.on("removewaypoint", (e) => {
+        console.log(e);
+        messages.value.push(
+          `<strong>${e.type}</strong>: waypoint removed at index <strong>${e.index}</strong>. Original event - <strong>${e.originalEvent.type}</strong>`,
+        );
+      });
+    });
+  });
+</script>
