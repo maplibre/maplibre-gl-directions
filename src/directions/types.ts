9e6f38ee9544cb19a8a6e7e5fafa9141ef2a1208
Matthew Mamonov <g.smellyshovel@gmail.com>
2022-05-10 02:39:10 +0400

    Fix setTimeout Infinity bug

--- src/directions/types.ts
+++ src/directions/types.ts
@@ -80,7 +80,7 @@ export interface MapLibreGlDirectionsConfiguration {
   /**
    * A timeout in ms after which a still-unresolved routing-request automatically gets aborted.
    *
-   * @default `Infinity` (no timeout)
+   * @default `null` (no timeout)
    *
    * @example
    * ```
@@ -88,7 +88,7 @@ export interface MapLibreGlDirectionsConfiguration {
    * requestTimeout: 5000
    * ```
    */
-  requestTimeout: number;
+  requestTimeout: number | null;
 
   /**
    * Whether to make a {@link https://docs.mapbox.com/api/navigation/http-post/|POST request} instead of a GET one.
@@ -271,7 +271,7 @@ export const MapLibreGlDirectionsDefaultConfiguration: Omit<MapLibreGlDirections
   api: "https://router.project-osrm.org/route/v1",
   profile: "driving",
   requestOptions: {},
-  requestTimeout: Infinity,
+  requestTimeout: null, // can't use Infinity here because of this: https://github.com/denysdovhan/wtfjs/issues/61#issuecomment-325321753
   makePostRequest: false,
   sourceName: "maplibre-gl-directions",
   pointsScalingFactor: 1,
