3b01839886c559ab3e1a23de6cba5c4041fa6472
Mattew Mamonov <g.smellyshovel@gmail.com>
2022-07-07 19:17:46 +0400

    WIP on testing

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,10 +1,18 @@
-import { test, expect } from "vitest";
-import maplibregl from "maplibre-gl";
-test("test", () => {
-  const container = document.createElement("div");
+import { describe, it, expect } from "vitest";
+import maplibregl, { type StyleSpecification } from "maplibre-gl";
+import MapLibreGlDirections from "../src/main";
+import style from "../demo/src/assets/map/style/style.json";
 
-  const map = new maplibregl.Map({
-    container,
-    style: "https://demotiles.maplibre.org/style.json",
+describe("public interface", () => {
+  it("creates an instance", () => {
+    function noOp() {}
+
+    const map = new maplibregl.Map({
+      container: document.createElement("div"),
+      style: style as StyleSpecification,
+    });
+
+    const directions = new MapLibreGlDirections(map);
+    expect(directions).toBeInstanceOf(MapLibreGlDirections);
   });
 });
--- vite.test.config.ts
+++ vite.test.config.ts
@@ -3,6 +3,17 @@ import { defineConfig } from "vitest/config";
 // https://vitejs.dev/config/
 export default defineConfig({
   test: {
-    environment: "jsdom",
+    environment: "happy-dom",
+    setupFiles: ["./vitest.setup.ts"],
+    deps: {
+      inline: ["vitest-canvas-mock"],
+    },
+    // For this config, check https://github.com/vitest-dev/vitest/issues/740
+    threads: false,
+    environmentOptions: {
+      jsdom: {
+        resources: "usable",
+      },
+    },
   },
 });
