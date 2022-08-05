37c24482c45b3f4aec838ced2325b3aa7be49790
Mattew Mamonov <g.smellyshovel@gmail.com>
2022-08-05 19:47:04 +0400

    Adapt for Vite 3

--- demo/src/router.ts
+++ demo/src/router.ts
@@ -1,18 +1,21 @@
+import type { ModuleNamespace } from "vite/types/hot";
 import Menu from "./Menu.svelte";
 
-export const examples = Object.entries(import.meta.globEager("./examples/**.svelte")).map(([path, component]) => {
-  const parsedFileName = path.match(/\/(\d+)\s([^/]+)\./);
-  const index = parseInt(parsedFileName[1]);
-  const name = parsedFileName[2];
+export const examples = Object.entries(import.meta.glob<ModuleNamespace>("./examples/**.svelte", { eager: true })).map(
+  ([path, component]) => {
+    const parsedFileName = path.match(/\/(\d+)\s([^/]+)\./);
+    const index = parseInt(parsedFileName[1]);
+    const name = parsedFileName[2];
 
-  return {
-    path: "/examples/" + name.toLowerCase().replaceAll(/\s/g, "-"),
-    index,
-    name: name,
-    component: component,
-    sourceUrl: `https://github.com/maplibre/maplibre-gl-directions/tree/main/demo/src/${path}`,
-  };
-});
+    return {
+      path: "/examples/" + name.toLowerCase().replaceAll(/\s/g, "-"),
+      index,
+      name: name,
+      component: component,
+      sourceUrl: `https://github.com/maplibre/maplibre-gl-directions/tree/main/demo/src/${path}`,
+    };
+  },
+);
 
 const routes = {};
 
