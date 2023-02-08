d9db981ca7c74d34a0e0c49f51ab649873c88e40
Matthew Mamonov <g.smellyshovel@gmail.com>
2023-02-09 00:48:48 +0700

    Bearings control - WIP on

--- src/controls/bearings/BearingsControl.svelte
+++ src/controls/bearings/BearingsControl.svelte
@@ -10,17 +10,51 @@
   directions.on("movewaypoint", onWaypointsChanged);
   directions.on("setwaypoints", onWaypointsChanged);
 
-  let waypoints;
+  let waypointsBearings: {
+    enabled: boolean;
+    angle: number;
+    degrees: number;
+  }[] = [];
 
   function onWaypointsChanged() {
-    waypoints = directions.waypoints;
+    waypointsBearings = directions.waypointsBearings.map((waypointBearing, index) => {
+      if (waypointsBearings[index]) return waypointsBearings[index];
+
+      return {
+        enabled: !!waypointBearing,
+        angle: waypointBearing ? waypointBearing[0] : 0,
+        degrees: waypointBearing ? waypointBearing[1] : 45,
+      };
+    });
   }
+
+  $: directions.waypointsBearings = waypointsBearings.map((waypointBearing) => {
+    return waypointBearing.enabled ? [waypointBearing.angle, waypointBearing.degrees] : undefined;
+  });
 </script>
 
-<div class="p-4 flex flex-col bg-white rounded">
-  {#if waypoints}
-    {#each waypoints as waypoint}
-      <div class="w-8 h-8 rounded-full bg-accent-500" />
-    {/each}
-  {/if}
+<div class="maplibregl-ctrl p-4 flex flex-col gap-4 bg-white rounded">
+  {#each waypointsBearings as waypointBearing}
+    <div class="flex items-center gap-2">
+      <input type="checkbox" bind:checked={waypointBearing.enabled} />
+      <input
+        type="number"
+        disabled={!waypointBearing.enabled}
+        bind:value={waypointBearing.angle}
+        min={configuration.angleMin}
+        max={configuration.angleMax}
+        step={configuration.angleStep}
+      />
+      <span>° ± </span>
+      <input
+        type="number"
+        disabled={!waypointBearing.enabled}
+        bind:value={waypointBearing.degrees}
+        min={configuration.degreesMin}
+        max={configuration.degreesMax}
+        step={configuration.degreesStep}
+      />
+      <span>°</span>
+    </div>
+  {/each}
 </div>
--- src/controls/bearings/types.ts
+++ src/controls/bearings/types.ts
@@ -1,12 +1,17 @@
 export interface BearingsControlConfiguration {
-  /**
-   * Fill-color for the loader. Any valid CSS-value.
-   *
-   * @default "#6d26d7"
-   */
-  fill: string;
+  angleMin: number;
+  angleMax: number;
+  angleStep: number;
+  degreesMin: number;
+  degreesMax: number;
+  degreesStep: number;
 }
 
 export const BearingsControlDefaultConfiguration: BearingsControlConfiguration = {
-  fill: "#6d26d7",
+  angleMin: 0,
+  angleMax: 359,
+  angleStep: 1,
+  degreesMin: 45,
+  degreesMax: 90,
+  degreesStep: 45,
 };
