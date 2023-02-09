2501a17c5262f424bfccfa2f6f4d215fa212b6d6
Matthew Mamonov <g.smellyshovel@gmail.com>
2023-02-09 20:55:28 +0700

    WIP on waypoint indication

--- src/controls/bearings/BearingsControl.svelte
+++ src/controls/bearings/BearingsControl.svelte
@@ -37,6 +37,22 @@
   {#each waypointsBearings as waypointBearing}
     <div class="flex items-center gap-2">
       <input type="checkbox" bind:checked={waypointBearing.enabled} />
+      <div>
+        <svg height="20" width="20" viewBox="0 0 20 20" style="width: 50px; height: 50px;">
+          <circle
+            r="5"
+            cx="10"
+            cy="10"
+            fill="transparent"
+            stroke="rgba(109, {waypointBearing.enabled ? '38, 215' : '109, 109'}, 0.65)"
+            stroke-width="10"
+            stroke-dasharray="calc({waypointBearing.degrees / 3.6} * 31.42 / 100) 31.42"
+            transform="rotate({-90 - waypointBearing.degrees / 2 + waypointBearing.angle})"
+            transform-origin="10 10"
+          />
+          <circle r="6" cx="10" cy="10" fill="rgb(109, {waypointBearing.enabled ? '38, 215' : '109, 109'})" />
+        </svg>
+      </div>
       <input
         type="number"
         disabled={!waypointBearing.enabled}
@@ -45,16 +61,21 @@
         max={configuration.angleMax}
         step={configuration.angleStep}
       />
-      <span>° ± </span>
-      <input
-        type="number"
-        disabled={!waypointBearing.enabled}
-        bind:value={waypointBearing.degrees}
-        min={configuration.degreesMin}
-        max={configuration.degreesMax}
-        step={configuration.degreesStep}
-      />
       <span>°</span>
+      <span>±</span>
+      {#if configuration.fixedDegrees}
+        <span>{configuration.fixedDegrees}°</span>
+      {:else}
+        <input
+          type="number"
+          disabled={!waypointBearing.enabled}
+          bind:value={waypointBearing.degrees}
+          min={configuration.degreesMin}
+          max={configuration.degreesMax}
+          step={configuration.degreesStep}
+        />
+        <span>°</span>
+      {/if}
     </div>
   {/each}
 </div>
