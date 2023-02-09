02df9f3b9bd3f1d2b35e8b692283e17cd0977d12
Matthew Mamonov <g.smellyshovel@gmail.com>
2023-02-09 20:54:13 +0700

    Change defaults and add fixed degrees option

--- src/controls/bearings/types.ts
+++ src/controls/bearings/types.ts
@@ -2,6 +2,7 @@ export interface BearingsControlConfiguration {
   angleMin: number;
   angleMax: number;
   angleStep: number;
+  fixedDegrees: number;
   degreesMin: number;
   degreesMax: number;
   degreesStep: number;
@@ -11,7 +12,8 @@ export const BearingsControlDefaultConfiguration: BearingsControlConfiguration =
   angleMin: 0,
   angleMax: 359,
   angleStep: 1,
-  degreesMin: 45,
-  degreesMax: 90,
-  degreesStep: 45,
+  fixedDegrees: 0,
+  degreesMin: 15,
+  degreesMax: 360,
+  degreesStep: 15,
 };
