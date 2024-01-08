d6548d6bf8d51f136dd718f4a24ad5ed786bbb55
Alexandra Petrova <bumblebee1906@gmail.com>
2024-01-08 07:33:51 +0400

    Profiles styles

--- demo/src/examples/14 Multiple profiles.svelte
+++ demo/src/examples/14 Multiple profiles.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import { examples } from "../router";
-  import { location } from "svelte-spa-router";
+  import { location, link } from "svelte-spa-router";
   import AppSidebar from "../components/AppSidebar.svelte";
   import maplibregl, { Map } from "maplibre-gl";
   import "maplibre-gl/dist/maplibre-gl.css";
   import style from "../assets/map/style/style.json?url";
   import MapLibreGlDirections from "@maplibre/maplibre-gl-directions";
+  import { shuffle } from "lodash";
 
   const meta = examples.find((example) => example.path === $location);
 
@@ -31,67 +32,52 @@
           alternatives: "true",
         },
       });
+
+      setPredefinedWaypoints();
     });
   });
 
+  const waypoints = [
+    [-74.21349031181673, 40.704951524836275],
+    [-74.19666749687558, 40.738517300855904],
+    [-74.16611177177786, 40.76634583723629],
+    [-74.07169801445302, 40.719004374548206],
+    [-73.99170381279369, 40.725509319065594],
+    [-73.98071748466812, 40.762445342516145],
+  ] satisfies [number, number][];
+  const profiles = ["car", "bike", "car", "car", "foot"];
+
   function setPredefinedWaypoints() {
-    directions?.setWaypoints([
-      [-74.21349031181673, 40.704951524836275],
-      [-74.19666749687558, 40.738517300855904],
-      [-74.16611177177786, 40.76634583723629],
-      [-74.07169801445302, 40.719004374548206],
-      [-73.99170381279369, 40.725509319065594],
-      [-73.98071748466812, 40.762445342516145],
-    ], ['car', 'foot', 'car', 'car', 'foot']);
+    directions?.setWaypoints(waypoints, profiles);
   }
 
-  function addRandomWaypoint() {
-    const bounds = map?.getBounds();
-    const center = map?.getCenter();
-
-    if (bounds && center) {
-      const xField = bounds.getEast() - bounds.getWest();
-      const yField = bounds.getSouth() - bounds.getNorth();
-
-      directions?.addWaypoint(
-        [center.lng - xField / 2 + xField * Math.random(), center.lat - yField / 2 + yField * Math.random()],
-        (directions?.waypoints.length * Math.random()) | 0,
-      );
-    }
-  }
-
-  function deleteRandomWaypoint() {
-    directions?.removeWaypoint((directions?.waypoints.length * Math.random()) | 0);
-  }
-
-  function clear() {
-    directions?.clear();
+  function shuffleWaypoints() {
+    directions?.setWaypoints(waypoints, shuffle(profiles));
   }
 </script>
 
 <AppSidebar>
   <span slot="title">{meta.name}</span>
+  <p>
+    This example showcases routing with multiple profiles. Segments corresponding to different profiles are displayed in
+    different colors. Plugin provides default styles for typical <a
+      href="https://project-osrm.org/docs/v5.5.1/api/#general-options">OSRM profiles</a
+    >: car, bike, foot. Styles can be changed per profile via general style customization approach (consult the
+    <a href="/examples/restyling" use:link>Restyling example</a>). In case different profiles are used you can similarly
+    style map features corresponding to each profile by targeting profile property of a feature (see
+    <a href="https://github.com/maplibre/maplibre-gl-directions/blob/main/src/directions/layers.ts">default styles</a>).
+  </p>
 
-  <small><strong>Note</strong> that interactivity is disabled for this example</small>
+  <small><strong>Note</strong> that interactivity is not supported for multiple profiles</small>
 
   <div class="flex flex-col gap-2">
-    <p>Set waypoints to a predefined set</p>
-    <button disabled={!directions} class="self-center" on:click={setPredefinedWaypoints}>Set Waypoints</button>
+    <p>Shuffles used profiles</p>
+    <button disabled={!directions} class="self-center" on:click={shuffleWaypoints}>Shuffle Profiles</button>
   </div>
 
   <div class="flex flex-col gap-2">
-    <p>Add a random waypoint at some random index</p>
-    <button disabled={!directions} class="self-center" on:click={addRandomWaypoint}>Add Waypoint</button>
-  </div>
-
-  <div class="flex flex-col gap-2">
-    <p>Delete a random waypoint</p>
-    <button disabled={!directions} class="self-center" on:click={deleteRandomWaypoint}>Delete Waypoint</button>
-  </div>
-
-  <div class="flex flex-col gap-2">
-    <p>Clear the map from all the stuff added by the plugin</p>
-    <button disabled={!directions} class="self-center" on:click={clear}>Clear</button>
+    <p>Reset to initial state</p>
+    <button disabled={!directions} class="self-center" on:click={setPredefinedWaypoints}>Reset</button>
   </div>
 </AppSidebar>
 
